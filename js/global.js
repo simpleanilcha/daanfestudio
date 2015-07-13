2c7425477a006f62d345bf81c935b3fb0d9c84f0
Anil Maharjan <simpleanilcha@gmail.com>
2015-07-13 22:44:41 +0545

    time changed.

--- js/global.js
+++ js/global.js
@@ -7,7 +7,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 $( function() {
 // Add background image
 	$.backstretch('images/road2.jpg');
-	var endDate = "May  4, 2015 15:03:25";
+	var endDate = "August  15, 2015 23:59:59";
 	$('.countdown.simple').countdown({ date: endDate });
 	$('.countdown.styled').countdown({
 	  date: endDate,
